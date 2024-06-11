71fa63fb28cb47ae952d5752ff101db0d716eca4
Mohammad Zafar <mzcoder7@gmail.com>
2024-06-11 17:52:02 +0100

    Set up parallel execution

--- AutomationFramework/src/test/java/testRunner/TestNGTestRunner.java
+++ AutomationFramework/src/test/java/testRunner/TestNGTestRunner.java
@@ -2,9 +2,14 @@ package testRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, plugin = {"pretty"}, dryRun = false
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
